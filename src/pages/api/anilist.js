a9038d8497d05fb19aeae995bc7b98f1245755ad
natoplus <natoplus.co@gmail.com>
2025-08-20 12:59:29 +0100

    watchtower changes

--- src/pages/api/anilist.js
+++ src/pages/api/anilist.js
@@ -1,5 +1,3 @@
-// /pages/api/anilist.js
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed. Use POST." });
@@ -18,7 +16,6 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-
     return res.status(200).json(data);
   } catch (err) {
     console.error("[AniList Proxy Error]", err);
