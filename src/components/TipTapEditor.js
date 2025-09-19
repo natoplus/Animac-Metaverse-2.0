f3aa89942598d70ea61a43d8d59af2398b5bea89
natoplus <natoplus.co@gmail.com>
2025-09-19 10:18:51 +0100

    latest updates

--- src/components/TipTapEditor.js
+++ src/components/TipTapEditor.js
@@ -4,7 +4,7 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "@tiptap/extension-link";
 import Highlight from "@tiptap/extension-highlight";
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyle } from '@tiptap/extension-text-style';
 import Color from '@tiptap/extension-color';
 import Underline from "@tiptap/extension-underline";
 import { TextAlign } from '@tiptap/extension-text-align';
